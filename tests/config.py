8dc75ec600c4ea2cc2bcb936b26e9a3ddcdd99d9
bissli <bissli@xyz.com>
2024-05-15 16:20:35 +0000

    add database keepalive checks

--- tests/config.py
+++ tests/config.py
@@ -10,9 +10,13 @@ postgres.username = 'postgres'
 postgres.password = 'postgres'
 postgres.port = 5432
 postgres.timeout = 30
+postgres.check_connection = True
+postgres.cleanup = True
 
 sqlite = Setting()
 sqlite.drivername = 'sqlite'
 sqlite.database = 'database.db'
+sqlite.check_connection = True
+sqlite.cleanup = True
 
 Setting.lock()
